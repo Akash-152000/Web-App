5fcdec380beb7fd94636af9b04246af7b2642304
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-06-11 19:51:13 +0530

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+data=[{"First_name":"Akash","Second_name":"Yadav","Phone":"123456"},{"First_name":"Bhavna","Second_name":"Soni","Phone":"98765"}]
+
+
 @app.route('/')
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", data=data)
 
 @app.route('/about')
 def about():
