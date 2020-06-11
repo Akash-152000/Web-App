ad2a4851574e7927cde663c5f7e12377db26de6f
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-06-11 20:20:06 +0530

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -10,7 +10,7 @@ def hello_world():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title="TITLE")
 
 if __name__=="__main__":
 	app.run(debug=True)
