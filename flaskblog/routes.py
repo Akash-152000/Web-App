4d3c88db65179565fc77b0c32e7fc6a6bb7a339d
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-06-17 17:35:09 +0530

    Update routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template,url_for,flash,redirect
 from flaskblog.forms import registrationForm,loginForm
 from flaskblog.models import User,Post
-from flaskblog import app
+from flaskblog import app,db,bcrypt
 
 data=[{"First_name":"Akash","Second_name":"Yadav","Phone":"123456","content":"COOL!!"},{"First_name":"Bhavna","Second_name":"Soni","Phone":"98765","content":"NICE!!"}]
 
@@ -19,8 +19,12 @@ def register():
 	form=registrationForm()
 
 	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!','Suceesss')
-		return redirect(url_for('home'))
+		hashed_password=bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+		user=User(username=form.username.data,email=form.email.data,password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash('Your account has been created! You are now able to login','Suceesss')
+		return redirect(url_for('login'))
 	return render_template("registration.html",title="Register",form=form)
 
 @app.route('/login',methods=['GET','POST'])
